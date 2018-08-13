3e3fb47cf648123e70ced3ec7ae2a89676ecf965
Fernando Mangas <fernandomangasmolteni@gmail.com>
2018-08-13 06:34:38 +0200

    Añado cosas al calendario

--- calendarionuevo.php
+++ calendarionuevo.php
@@ -34,38 +34,69 @@ and open the template in the editor.
         $mes=date("n");
         $anio=date("Y");
         $diaActual=date("j");
+       
+        //array diccionario con colores
+        $colores = array("red","green","yellow","pink");
         
         //posicion de la celda en la que empieza y final
         $diaSemana=date("w",mktime(0,0,0,$mes,1,$anio));
         $ultimoDiaMes=date("d",(mktime(0,0,0,$mes+1,1,$anio)-1));
         $last_cell=$diaSemana+$ultimoDiaMes;
-        
+
         //conexión y extracción de datos en un array asociativo
         $conexion = conexionMysql(servidor_ip,usuario,password,nombrebase);       
         
         $day = 1;
         $m = 0;
+        $posicion = null;
         
         //extraemos datos y lo almacenamos 
         $fechas = consulta($conexion,sql);
         
+        //$i es la posicion que empieza en el calendario (casilla)
         for($i=1;$i<=42;$i++){
  
             if($i>=$diaSemana && $i<$last_cell){
+                        
+                //calculamos la posicion actual en la que empieza el evento
+                $posicionActual = ($diaSemana+($i-1)-2);
                 
-                if($m<count($fechas)){
-                    $diaini = intval(date_format(new DateTime($fechas[$m]["fecha_ini"]),'d'));
-                    $diafin = intval(date_format(new DateTime($fechas[$m]["fecha_fin"]),'d'));
+                //almacenamos 0 si no hay eventos
+                $diaini[$i-1] = 0;
+                $diafin[$i-1] = 0;
+                
+                echo "diaA=$day - diaB=".intval(date_format(
+                        new DateTime($fechas[$m]["fecha_ini"]),'d'))." - mes=$mes en la posicion=$posicionActual</br>";
+                
+                
 
+                //almacenamos eventos si estamos en el mismo mes
+                if(($day==intval(date_format(
+                        new DateTime($fechas[$m]["fecha_ini"]),'d'))) &&
+                        ($mes==intval(date_format(
+                        new DateTime($fechas[$m]["fecha_ini"]),'m')))){
+                    
+                    
+                   echo "almacena valor</br>"; 
+                    
+                    //almacenamos el dia inicial y final de los eventos a añadir
+                    $diaini[$i-1] = intval(date_format(new DateTime($fechas[$m]["fecha_ini"]),'d'));
+                    $diafin[$i-1] = intval(date_format(new DateTime($fechas[$m]["fecha_fin"]),'d'));
+                    
+                    //para que no se vaya de rango contamos elementos del array
+                    if($m<count($fechas)-1){
+                        $m++;
+                    }
                 }
                 
-                
-                echo mktime(0,0,0,$mes,$diaini,$ani);
-                if(date("w",mktime(0,0,0,$mes,$diaini,$anio))>=$i && $i<=date("w",mktime(0,0,0,$mes,$diafin,$anio))){
+
+                //si esta en un rango de dias lo señalo en color en el calendario
+                if(($i>=$posicionActual) && (($i<=($posicionActual+$diafin[$i-1]-1)
+                        ))){
 
                     $html.="<td style='background:red'>$day</td>";
                         
-                    $m++;
+                    
                 }
                 else{
                     $html.="<td>$day</td>";
@@ -82,7 +113,7 @@ and open the template in the editor.
             if($i%7==0){
                 $html.="<tr></tr>";
             }
-       
+          
         }
 
         //incrustamos la tabla
